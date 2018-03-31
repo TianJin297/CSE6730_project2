8a654c3aecc211920e9595d7dc85612acc29ee66
Hu, Yuqing <huyuqing1991@gmail.com>
2018-03-31 15:27:52 -0400

    added event

--- src/edu/gatech/Event.java
+++ src/edu/gatech/Event.java
@@ -1,4 +1,54 @@
 package edu.gatech;
+public class Event implements Comparable<Event> {
+    private EventHandler m_handler;
+    private double m_time;
+    private double m_delay;
+    private int m_eventId;
+    private int m_eventType;
+    static private int m_nextId = 0;
 
-public class Event {
+    Event() {
+        m_eventId = m_nextId++;
+    }
+
+    Event(double delay, EventHandler handler, int eventType) {
+        this();
+        m_delay = delay;
+        m_handler = handler;
+        m_eventType = eventType;
+    }
+
+    public int getId() {
+        return m_eventId;
+    }
+
+    public double getTime() {
+        return m_time;
+    }
+
+    public double getDelay() { return m_delay; }
+
+    public void setTime(double time) {
+        m_time = time;
+    }
+
+    public EventHandler getHandler() {
+        return m_handler;
+    }
+
+    public int getType() { return m_eventType; }
+
+    public void setHandler(EventHandler handler) {
+        m_handler = handler;
+    }
+
+    @Override
+    public int compareTo(final Event ev) {
+        int timeCmp = Double.compare(m_time, ev.getTime());
+        if(timeCmp != 0) {
+            return timeCmp;
+        }
+        else
+            return Integer.compare(m_eventId, ev.getId());
+    }
 }
